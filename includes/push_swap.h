b1623e0badd2b2a98f3fcf7794980e71c726a788
Lucas <lucas@MacBook-Pro-de-Lucas-2.local>
2024-02-09 18:06:30 +0100

    starting algorithm + stack init

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lumaret <lumaret@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lucas <lucas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 02:22:48 by lumaret           #+#    #+#             */
-/*   Updated: 2024/02/08 20:06:02 by lumaret          ###   ########.fr       */
+/*   Updated: 2024/02/09 18:03:26 by lucas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,32 @@ typedef struct s_stack_node
 }	t_stack_node;
 
 //Handle errors
+int	syntaxe_error(char *str_n);
+int	doublons_error(t_stack_node *a, int n);
+void	free_stack(t_stack_node **stack);
+void	print_error(t_stack_node **a);
 
 //stack init
+static long	ft_atol(const char *s);
+void	init_stack_a(t_stack_node **a, char **argv)
+t_stack_node *the_cheapest(t_stack_node *stack)
+void	for_each_push(t_stack_node **stack, t_stack_node *top_node, char name)
 
 //nodes init
+static void	add_node(t_stack_node **stack, int n);
 
 //stack utils
+int	stack_len(t_stack_node *stack);
+t_stack_node	*point_on_last(t_stack_node *stack);
+t_stack_node	*min_o_stack(t_stack_node *stack);
+t_stack_node	*max_o_stack(t_stack_node *stack);
+char	**ps_split(char *s, char c);
 
 //commands
 
 //alogrithms
+void	current_index(t_stack_node *stack);
+static void	set_target_a(t_stack_node *a, t_stack_node *b);
+static void	cost_analysis_a(t_stack_node *a, t_stack_node *b);
 
 #endif
