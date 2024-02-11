5e8d79b43252918d91209c4fa1774e8501357ee5
lumaret <lumaret@student.42.fr>
2024-02-11 02:32:18 +0100

    switch okay

--- src/commandes/all_swap.c
+++ src/commandes/all_swap.c
@@ -6,7 +6,7 @@
 /*   By: lumaret <lumaret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 00:06:50 by lumaret           #+#    #+#             */
-/*   Updated: 2024/02/11 01:21:53 by lumaret          ###   ########.fr       */
+/*   Updated: 2024/02/11 02:31:49 by lumaret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,31 @@ static void	swap(t_stack_node **head)
 	temp = (*head)->next;
 	(*head)->next = temp->next;
 	(*head)->prev = temp;
-
 	if (temp->next != NULL)
 		temp->next->prev = *head;
-
 	temp->next = *head;
 	temp->prev = NULL;
 	*head = temp;
+}
+
+t_stack_node	sa(t_stack_node **a, bool print)
+{
+	swap(a);
+	if (!print)
+		ft_printf("sa\n");
+}
+
+t_stack_node	sa(t_stack_node **b, bool print)
+{
+	swap(b);
+	if (!print)
+		ft_printf("sb\n");
+}
+
+t_stack_node	ss(t_stack_node **a, t_stack_node **b, bool print)
+{
+	swap(a);
+	swap(b);
+	if (!print)
+		ft_printf("ss\n");
 }
